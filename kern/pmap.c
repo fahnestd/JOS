29a080fa252fb413a313d1c0c5c8138c2ae4f31e
fahnestd <fahnestd@oregonstate.edu>
2024-05-18 18:32:09 -0400

    100%

--- kern/pmap.c
+++ kern/pmap.c
@@ -588,7 +588,11 @@ user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 	for (uint32_t i = start; i < end; i += PGSIZE) {
 		pte_t *pte = pgdir_walk(env->env_pgdir, (void *) i, 0);
 		if (!pte || (i >= ULIM) || !(*pte & PTE_P) || ((*pte & perm) != perm)) {
-			user_mem_check_addr = (i);
+			if (i < (uint32_t) va) {
+				user_mem_check_addr = (uint32_t)va;
+			} else {
+				user_mem_check_addr = (i);
+			}
 			return -E_FAULT;
 		}
 	}
